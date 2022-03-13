615e247f066ab51eb5340e9a9d68b9aaadc77a36
MatchaSoda <1700120073@qq.com>
2022-03-14 00:08:33 +0800

    '添加了修改后的第1题'

---
+++ 0001.两数之和.cpp
@@ -0,0 +1,14 @@
+class Solution {
+ public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;  // value index
+    for (int i = 0; i < nums.size(); i++) {
+      if (!map.count(nums[i])) {
+        map.insert({target - nums[i], i});
+      } else {
+        return {i, map[nums[i]]};
+      }
+    }
+    return {};
+  }
+};
