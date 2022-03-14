600fec5da5242fe95ba83aaf9de96a2c1fd96d65
MatchaSoda <1700120073@qq.com>
2022-03-15 00:31:39 +0800

    使用go fmt格式化了第一题

--- 0001.两数之和.go
+++ 0001.两数之和.go
@@ -1,10 +1,12 @@
+package leetcode0001
+
 func twoSum(nums []int, target int) []int {
-    hashTable := map[int]int{}
-    for i, x := range nums {
-        if p, ok := hashTable[target - x]; ok {
-            return []int {p, i}
-        }
-        hashTable[x] = i
-    }
-    return nil;
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
 }
