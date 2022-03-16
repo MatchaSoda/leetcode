91d68db3a13889e66f7cd608b914dd9c7eb7bb09
MatchaSoda <68466082+MatchaSoda@users.noreply.github.com>
2022-03-16 16:37:46 +0800

    leetcode question13:romanToInt (#8)
    * leetcode question13:romanToInt
    
    * fix

---
+++ p13.go
@@ -0,0 +1,15 @@
+package leetcode
+
+var romanCharValues = map[byte]int{'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+func romanToInt(s string) int {
+	result := 0
+	for charIndex, currentValue := range s {
+		if charIndex < len(s)-1 && currentValue < romanCharValues[s[charIndex+1]] {
+			result -= currentValue
+		} else {
+			result += currentValue
+		}
+	}
+	return result
+}
