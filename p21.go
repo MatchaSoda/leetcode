4fe344811b74fcabf483400d7c21587f9f84747b
MatchaSoda <1700120073@qq.com>
2022-03-20 20:48:29 +0800

    leetcode question21: mergeTwoLists

---
+++ p21.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummyNode := &ListNode{}
+	currentNode := dummyNode
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			currentNode.Next = list1
+			list1 = list1.Next
+		} else {
+			currentNode.Next = list2
+			list2 = list2.Next
+		}
+		currentNode = currentNode.Next
+	}
+	if list1 == nil {
+		currentNode.Next = list2
+	} else {
+		currentNode.Next = list1
+	}
+	return dummyNode.Next
+}
